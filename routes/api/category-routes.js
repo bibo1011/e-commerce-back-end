d449ff379b8feacb32d4f58bb422c2922b41a628
Blue Abad <bibo1011@gmail.com>
2020-10-15 22:48:20 -0700

    add category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,24 +4,72 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  Category.findAll({
+    include: [Product]
+  }).then(dbCategory => {
+    res.json(dbCategory);
+  })
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  Category.findOne({
+    include: [Product]
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategory =>{
+    if (!dbCategory) {
+      return res.status(404).json({message: 'not found'});
+    } 
+    return res.json(dbCategory);
+  })
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
+  Category.create({
+    category: req.body.category_name
+  }).then(dbCategory => {
+    res.json(dbCategory);
+  }).catch(err => {
+    res.status(500).json(err);
+  })
   // create a new category
 });
 
 router.put('/:id', (req, res) => {
+  Category.update({
+    category: req.body.category_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategory =>{
+    if (!dbCategory) {
+      return res.status(404).json({message: 'not found'});
+    } 
+    return res.json(dbCategory);
+  })
   // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategory =>{
+    if (!dbCategory) {
+      return res.status(404).json({message: 'not found'});
+    } 
+    return res.json(dbCategory);
+  })
   // delete a category by its `id` value
 });
 
